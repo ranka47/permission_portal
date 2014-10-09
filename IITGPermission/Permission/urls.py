aa6bad478b6306751c95acfd45ccf9f3a42aab31
Jayadeep <karnatijayadeep@bitbucket.org>
2014-10-09 22:52:55 +0530

    Added User Authentication System. Access with locahost:8000/Permission/

---
+++ IITGPermission/Permission/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+
+from Permission import views
+
+urlpatterns = [
+        url(r'^$', views.login, name='index'),
+		url('auth/$', views.auth_view),
+		url('logout/$', views.logout),
+		url('loggedin/$', views.loggedin),
+		url('invalid/$', views.invalid_login),
+]
